b80a28253f9b6e29727b376d230b0b38112d3d42
sijin1017 <trsj1017@naver.com>
2024-09-18 13:37:59 +0900

    기본 setting

---
+++ src/test/java/groom/backend/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package groom.backend.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
